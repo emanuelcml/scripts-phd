664c4465614ba605b44032a270c04f3a5355ee90
emanuel <emanuel.cml@gmail.com>
2024-01-25 15:23:22 -0300

    Dash ETOT version with rangeslider

--- dash-cpmd/graphics_sim.py
+++ dash-cpmd/graphics_sim.py
@@ -10,24 +10,29 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 app.layout = html.Div([
     html.H1(children='Dados de Energia Total', style={'textAlign': 'center'}),
-    dcc.Input(placeholder='Plotar a partir de ...',
-              type='number', min=0, max= int(max(evp.data['nfi'])),
-              id='input-nfi'),
+    dcc.RangeSlider(min(evp.data['nfi']),
+                    max(evp.data['nfi']),
+                    step=1,
+                    marks={i: str(i) for i in range(int(min(evp.data['nfi'])),
+                    int(max(evp.data['nfi'])), 500)},
+                    allowCross=False,
+                    id='nfi-range-slider'),
+    html.Div(id='output-container-range-slider'),
     dcc.Graph(id='graph-content')
 ])
 
 @callback(
     Output('graph-content', 'figure'),
-    # Input('nfi-range-slider', 'value')
-    Input('input-nfi', 'value')
+    Output('output-container-range-slider', 'children'),
+    Input('nfi-range-slider', 'value')
 )
-def update_graph(value=0):
+def update_graph(value):
     if value is None:
-        value = 0
+        value = [int(min(evp.data['nfi'])), int(max(evp.data['nfi']))]
 
-    plot_range = list(map(float, range( value, int(max(evp.data.nfi)) )))
-    dff = evp.data.iloc[plot_range, :]
-    return px.line(dff, x='nfi', y='etot')
+    texto_debug = 'Intervalo de passos de execução: "{}"'.format(value)
+    dff = evp.data.iloc[value[0]:value[-1], :]
+    return px.line(dff, x='nfi', y='etot'), texto_debug
 
 if __name__ == '__main__':
     app.run(debug=True)
